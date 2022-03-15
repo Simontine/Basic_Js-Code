62c9487dff171459bed9cc5dcabcbcce4418d093
DA learner <dalearner@DAs-MacBook-Air.local>
2022-03-15 16:22:46 +0200

    Second Commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express(); //Basic ex server
 const cors = require('cors');
 const port = process.env.PORT || 8081;
+const user = require('./example.controller');
+const userRoute  = require('./userRoute');
 
 app.use(cors());// Cross origin res sharing
 app.use(express.json());// Allowing our server to accept json data
@@ -10,14 +12,14 @@ app.get('/',(request,response)=>{// Send a response to the user
     console.log('Basic backend is Biggy');
     response.status(200).send('Basic backend is running');
 });
-app.post('/',(request,response)=>{// Request comes from the user 
-    const data = request.body;
-    console.log('data',data);
-    response.status(204).send(data);
-});
-app.delete('/',()=>{
+// app.post('/',(request,response)=>{// Request comes from the user 
+//     const data = request.body;
+//     console.log('data',data);
+//     response.status(204).send(data);
+// });
 
-})
+app.use('/user',userRoute);
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
